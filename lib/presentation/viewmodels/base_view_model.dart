15db5c34b3136fe6e47799fc67a05cc0df69bcc2
Priscilla Achizue <priscillaachizue@gmail.com>
2022-03-14 12:09:25 +0100

    Base view model

---
+++ lib/presentation/viewmodels/base_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:joker/services/get_joke_service.dart';
+import 'package:joker/utils/locator.dart';
+///Base view model with shared dependencies injected.
+///All view models must extends this class.
+class BaseViewModel extends ChangeNotifier {
+  late JokeService jokeService;
+
+  BaseViewModel(
+      {JokeService? jokeService}) {
+    this.jokeService = jokeService?? locator();
+  }
+
+  bool _loading = false;
+  bool get loading => _loading;
+  void toggleLoading(bool val) {
+    _loading = val;
+    notifyListeners();
+  }
+}
